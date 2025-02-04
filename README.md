3edc92cd5d6ac0459488337d1f26d2a57c811ccf
amanattrish <iam.amanattrish@gmail.com>
2025-02-04 18:30:47 +0530

    added logic for doc read

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # AI
+POC for document (Image, PDF, Word doc) summary
+
+### Environment variables
+
+First, copy the `.env.example` file and rename it as `.env`..
+Add OpenAI API key in `.env` file at the root directory. As `OPENAI_API_KEY`.
+
+
+### To run the docker container:
+
+1. Build the image by `docker build . -t test-model:latest`
+2. Run the container `docker container run -p 8000:8000 test-model`
+
+
+### Request cURL
+
+```
+curl --location 'localhost:8000/chat-completion' \
+--form 'file=@"/path/to/file"' \
+--form 'prompt="Summerize the given document and include as much detail as possible."'
+```
---
+++ main.py
@@ -0,0 +1,110 @@
+from fastapi import FastAPI, UploadFile, File, Form, HTTPException
+from fastapi.responses import FileResponse
+import openai
+from dotenv import load_dotenv
+from pypdf import PdfReader
+from typing import Annotated
+from docx import Document
+import base64
+import tempfile
+
+load_dotenv()
+app = FastAPI()
+client = openai.OpenAI()
+
+MODEL = 'gpt-4o-mini'
+SUPPORTED_EXTENSIONS = set(
+    ['doc', 'dot', 'docx', 'dotx', 'docm', 'dotm', 'pdf', 'png', 'jpeg', 'jpg'])
+SYSTEM_PROMPT = "You are an assistant who analysis documents provided which are of type image, pdf or word. The pdf and word document will be given to after extracting the text from them.\nThe document content will be specified by a different heading for you to differentiate between the document content and user prompt.\nIgnore all the formatting, spacing issues and line breaks, do not bring it up to the user or in the final response and correct them silently yourself.\nYou will give your response in markdown."
+
+# Health checkup end point
+@app.get("/")
+def health():
+    return {
+        "status": "success",
+        "message": "Backend Healthy"
+    }
+
+# Chat completion end point
+@app.post("/chat-completion")
+def chatCompletion(file: Annotated[UploadFile, File()], prompt: Annotated[str, Form()]):
+    documentText = ''
+    b64 = None
+    fileExt = file.filename.split('.')[-1].lower()
+    if fileExt not in SUPPORTED_EXTENSIONS:
+        return {
+            "status": "failed",
+            "message": "File type not supported"
+        }
+    if fileExt == 'pdf':
+        reader = PdfReader(file.file)
+        for page in reader.pages:
+            documentText += page.extract_text(extraction_mode='layout')
+            documentText += '\n\n'
+        if len(documentText.strip()) == 0:
+            raise HTTPException(
+                status_code=400, detail="Unable to parse text from the given document or the document does not contain any text.")
+    elif fileExt in ['png', 'jpeg', 'jpg']:
+        with tempfile.TemporaryDirectory() as temp_dir:
+            path = f"{temp_dir}/filename.{fileExt}"
+            with open(path, 'wb') as img:
+                img.write(file.file.read())
+            with open(path, 'rb') as img:
+                b64 = base64.b64encode(img.read()).decode("utf-8")
+    else:
+        document = Document(file.file)
+        for paragraph in document.paragraphs:
+            documentText += paragraph.text if paragraph and paragraph.text else ""
+        # text = ''
+        # for elem in document.tables:
+        #     textRow = set()
+        #     for i, row in enumerate(elem.columns):
+        #         for cell in row.cells:
+        #             textRow.add(f"{cell.text}\n")
+        #     text += f"{' '.join(list(textRow))}\n\n"
+        # print(text)
+
+    userContent = [
+        {
+            "type": "text",
+            "text": ""
+        }
+    ]
+
+    if len(documentText) > 0:
+        userContent[0]['text'] = f"User prompt:\n{prompt}\n\nThis is document content: \n{documentText}"
+    elif b64 is not None:
+        userContent[0]['text'] = "I will be attaching an image"
+
+    if b64 is not None:
+        userContent.append({
+            "type": "image_url",
+            "image_url": {"url": f"https://i.ibb.co/rxBnBqQ/image1-1.jpg"}
+        })
+
+    response = client.chat.completions.create(
+        model=MODEL,
+        messages=[
+            {
+                "role": "system",
+                "content": SYSTEM_PROMPT
+            },
+            {
+                "role": "user",
+                "content": userContent
+            }
+        ]
+    )
+    print(response.choices[0].message.content)
+
+    # pdf = MarkdownPdf(toc_level=2)
+    # pdf.add_section(Section(response.choices[0].message.content))
+    # pdf.save(f"response.pdf")
+    # response = FileResponse(f"response.pdf",
+    #                         media_type="application/pdf")
+
+    return {
+        "status": "success",
+        "prompt": prompt,
+        "response": response.choices[0].message.content
+    }
