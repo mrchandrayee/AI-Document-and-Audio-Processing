d465f2cbd26a5286d71ec4a7bac62bb2882dbb60
amanattrish <iam.amanattrish@gmail.com>
2025-02-05 18:11:57 +0530

    changes as expected

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# AI
+# ff-poc
 POC for document (Image, PDF, Word doc) summary
 
 ### Environment variables
@@ -16,12 +16,18 @@ Add OpenAI API key in `.env` file at the root directory. As `OPENAI_API_KEY`.
 ### Request cURL
 
 ```
-curl --location 'localhost:8000/chat-completion' \
---form 'file=@"/path/to/file"' \
---form 'prompt="Summerize the given document and include as much detail as possible."'
+curl --location 'http://localhost:8000/chat-completion' \
+--form 'file=@"/Users/ayushagrawal/Downloads/pexels-huynhthanhphong-6864554.jpg"' \
+--form 'prompt="What is happening in the image provided"' \
+--form 'response_type="pdf"' \
+--form 'model_name="gpt-4o-mini"'
 ```
+
+`model_name` can be 'gpt-4o' or 'gpt-4o-mini'
+`response_type` can be 'string' or 'pdf'
 
 ### TODO
 - Fix image erros - it is throwing erros when images are served
 - Input and output file shoule be optional
 - Check if we can support rtf, xlsx, xls file extensions
+> This needs excel parsing to make sure the rows and columns relationship is maintained in a way so that when we pass the raw data to the model it have the context about the table in excel.
--- main.py
+++ main.py
@@ -7,12 +7,22 @@ from typing import Annotated
 from docx import Document
 import base64
 import tempfile
+from enum import Enum
+from markdown_pdf import MarkdownPdf, Section
+load_dotenv(override=True)
 
-load_dotenv()
 app = FastAPI()
 client = openai.OpenAI()
 
-MODEL = 'gpt-4o-mini'
+class ResponseType(str, Enum):
+    pdf = "pdf"
+    string = "string"
+
+class ModelType(str, Enum):
+    gpt4o = "gpt-4o"
+    gpt4omini = "gpt-4o-mini"
+
+MODEL = 'gpt-4o'
 SUPPORTED_EXTENSIONS = set(
     ['doc', 'dot', 'docx', 'dotx', 'docm', 'dotm', 'pdf', 'png', 'jpeg', 'jpg'])
 SYSTEM_PROMPT = "You are an assistant who analysis documents provided which are of type image, pdf or word. The pdf and word document will be given to after extracting the text from them.\nThe document content will be specified by a different heading for you to differentiate between the document content and user prompt.\nIgnore all the formatting, spacing issues and line breaks, do not bring it up to the user or in the final response and correct them silently yourself.\nYou will give your response in markdown."
@@ -27,42 +37,45 @@ def health():
 
 # Chat completion end point
 @app.post("/chat-completion")
-def chatCompletion(file: Annotated[UploadFile, File()], prompt: Annotated[str, Form()]):
+def chatCompletion(prompt: Annotated[str, Form()], response_type: Annotated[ResponseType, Form()] = ResponseType.string, model_name: Annotated[ModelType, Form()] = ModelType.gpt4omini, file: Annotated[UploadFile | None, File()] = None):
+    MODEL = model_name
+    print(MODEL)
     documentText = ''
     b64 = None
-    fileExt = file.filename.split('.')[-1].lower()
-    if fileExt not in SUPPORTED_EXTENSIONS:
-        return {
-            "status": "failed",
-            "message": "File type not supported"
-        }
-    if fileExt == 'pdf':
-        reader = PdfReader(file.file)
-        for page in reader.pages:
-            documentText += page.extract_text(extraction_mode='layout')
-            documentText += '\n\n'
-        if len(documentText.strip()) == 0:
-            raise HTTPException(
-                status_code=400, detail="Unable to parse text from the given document or the document does not contain any text.")
-    elif fileExt in ['png', 'jpeg', 'jpg']:
-        with tempfile.TemporaryDirectory() as temp_dir:
-            path = f"{temp_dir}/filename.{fileExt}"
-            with open(path, 'wb') as img:
-                img.write(file.file.read())
-            with open(path, 'rb') as img:
-                b64 = base64.b64encode(img.read()).decode("utf-8")
-    else:
-        document = Document(file.file)
-        for paragraph in document.paragraphs:
-            documentText += paragraph.text if paragraph and paragraph.text else ""
-        # text = ''
-        # for elem in document.tables:
-        #     textRow = set()
-        #     for i, row in enumerate(elem.columns):
-        #         for cell in row.cells:
-        #             textRow.add(f"{cell.text}\n")
-        #     text += f"{' '.join(list(textRow))}\n\n"
-        # print(text)
+    if file is not None:
+        fileExt = file.filename.split('.')[-1].lower()
+        if fileExt not in SUPPORTED_EXTENSIONS:
+            return {
+                "status": "failed",
+                "message": "File type not supported"
+            }
+        if fileExt == 'pdf':
+            reader = PdfReader(file.file)
+            for page in reader.pages:
+                documentText += page.extract_text(extraction_mode='layout')
+                documentText += '\n\n'
+            if len(documentText.strip()) == 0:
+                raise HTTPException(
+                    status_code=400, detail="Unable to parse text from the given document or the document does not contain any text.")
+        elif fileExt in ['png', 'jpeg', 'jpg']:
+            with tempfile.TemporaryDirectory() as temp_dir:
+                path = f"{temp_dir}/filename.{fileExt}"
+                with open(path, 'wb') as img:
+                    img.write(file.file.read())
+                with open(path, 'rb') as img:
+                    b64 = base64.b64encode(img.read()).decode("utf-8")
+        else:
+            document = Document(file.file)
+            for paragraph in document.paragraphs:
+                documentText += paragraph.text if paragraph and paragraph.text else ""
+            # text = ''
+            # for elem in document.tables:
+            #     textRow = set()
+            #     for i, row in enumerate(elem.columns):
+            #         for cell in row.cells:
+            #             textRow.add(f"{cell.text}\n")
+            #     text += f"{' '.join(list(textRow))}\n\n"
+            # print(text)
 
     userContent = [
         {
@@ -76,10 +89,13 @@ def chatCompletion(file: Annotated[UploadFile, File()], prompt: Annotated[str, F
     elif b64 is not None:
         userContent[0]['text'] = "I will be attaching an image"
 
+    if file is None:
+        userContent[0]['text'] = prompt
+
     if b64 is not None:
         userContent.append({
             "type": "image_url",
-            "image_url": {"url": f"https://i.ibb.co/rxBnBqQ/image1-1.jpg"}
+            "image_url": {"url": f"data:image/jpeg;base64,{b64}"}
         })
 
     response = client.chat.completions.create(
@@ -97,11 +113,16 @@ def chatCompletion(file: Annotated[UploadFile, File()], prompt: Annotated[str, F
     )
     print(response.choices[0].message.content)
 
-    # pdf = MarkdownPdf(toc_level=2)
-    # pdf.add_section(Section(response.choices[0].message.content))
-    # pdf.save(f"response.pdf")
-    # response = FileResponse(f"response.pdf",
-    #                         media_type="application/pdf")
+    if response_type is ResponseType.pdf:
+        pdf = MarkdownPdf()
+        content = response.choices[0].message.content
+        # while breakWords <= len(content):
+        pdf.add_section(Section(content))
+        #     print(offset)
+        #     breakWords+=offset
+        pdf.save(f"response.pdf")
+        return FileResponse(f"response.pdf",
+                            media_type="application/pdf")
 
     return {
         "status": "success",
